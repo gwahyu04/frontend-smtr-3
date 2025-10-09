5155d5dc722f4a4e8c9d7655633fc92577f61133
Wahyu <wahyupisang6@gmail.com>
2025-10-09 08:48:58 +0700

    Add files via upload

---
+++ pertemuan4/bola.js
@@ -0,0 +1,5 @@
+import { PHI } from './math-constants.js';
+
+const volume = (radius) => 4 * PHI * radius ** 3;
+
+export default volume;
---
+++ pertemuan4/overflow.js
@@ -0,0 +1,4 @@
+const penjumlahan = (a,b) => a + b
+let hasil = penjumlahan(3,4,5,6)
+
+alert('Hasil penjumlahan = $ {hasil}')
